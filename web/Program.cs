2397624400975e7c7e216f10c1a7db1e58a35355
Breno Trindade <breno.trindade2011@gmail.com>
2025-09-16 19:22:02 -0300

    feat(login): update login process

--- web/Program.cs
+++ web/Program.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using AgendaUni.Web;
 using AgendaUni.Web.Services;
 using System.Net.Http.Json;
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -14,6 +16,9 @@ var s = builder.Configuration["ApiBaseUrl"];
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["ApiBaseUrl"] ?? "http://localhost:8080")});
 
+builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
 
 builder.Services.AddScoped<EventService>();
 builder.Services.AddScoped<AuthService>();
